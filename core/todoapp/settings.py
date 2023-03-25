628fb534c5ff6e16a5f09de0912b89b2abcf36c6
Jordano98 <mohammad.dehghan218@gmail.com>
2023-03-25 15:50:31 +0430

    using environments

--- core/todoapp/settings.py
+++ core/todoapp/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--0v*!b(gu^zz&9w*rc61c8u+qymlx8zn70d*#3kp)xs^%8n$@h'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')],default='192.168.99.100')
 
