29ce27c87390800a0ded601d16f28cc2676ebcf8
Jordano Bruno <100492667+Jordano98@users.noreply.github.com>
2023-03-31 01:28:45 +0430

    add swagger and recod documentation

--- core/core/settings.py
+++ core/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')],default='127.0.0.1')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')],default="0.0.0.0")
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "task_list",
     'rest_framework',
     'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
